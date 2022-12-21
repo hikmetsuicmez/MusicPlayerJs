d12929501dc6d77c85855537fc74bbd615d5d1f2
Hikmet Suicmez <suicmezhikmet1@gmail.com>
2022-12-21 16:17:29 +0300

    Controls, progress Bar, Time Control

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+
 const container = document.querySelector(".container");
 const image = document.querySelector("#music-image");
 const title = document.querySelector("#music-details .title");
@@ -6,6 +7,9 @@ const singer = document.querySelector("#music-details .singer");
 const prev = document.querySelector("#controls #prev");
 const play = document.querySelector("#controls #play");
 const next = document.querySelector("#controls #next");
+const duration = document.querySelector("#duration");
+const currentTime = document.querySelector("#current-time");
+const progressBar = document.querySelector("#progress-bar");
 
 const player = new MusicPlayer(MusicList);
 
@@ -13,17 +17,75 @@ let music = player.getMusic();
 
 
 window.addEventListener("load", () => {
-    let music = player.getMusic();
-    displayMusic(music);
-})
+  let music = player.getMusic();
+  displayMusic(music);
+});
 
 function displayMusic(music) {
-    title.innerText = music.getName();
-    singer.innerText = music.singer;
-    image.src = "img/" + music.img;
-    audio.src = "mp3/" + music.file;
+  title.innerText = music.getName();
+  singer.innerText = music.singer;
+  image.src = "img/" + music.img;
+  audio.src = "mp3/" + music.file;
 }
 
 play.addEventListener("click", () => {
-    audio.play();
-})
+  const isMusicPlay = container.classList.contains("playing");
+  isMusicPlay ? pauseMusic() : playMusic();
+});
+
+prev.addEventListener("click", () => {
+  prevMusic();
+});
+
+next.addEventListener("click", () => {
+  nextMusic();
+});
+
+function prevMusic() {
+  player.previous();
+  let music = player.getMusic();
+  displayMusic(music);
+  playMusic();
+}
+
+function nextMusic() {
+  player.next();
+  let music = player.getMusic();
+  displayMusic(music);
+  playMusic();
+}
+
+function pauseMusic() {
+  container.classList.remove("playing");
+  play.classList = "fa-solid fa-play";
+  audio.pause();
+}
+
+function playMusic() {
+  container.classList.add("playing");
+  play.classList = "fa-solid fa-pause";
+  audio.play();
+}
+
+const calculateTime = (toplamSaniye) => {
+  const dakika = Math.floor(toplamSaniye / 60);
+  const saniye = Math.floor(toplamSaniye % 60);
+  const guncellenenSaniye = saniye < 10 ? `0${saniye}` : `${saniye}`;
+  const sonuc = `${dakika}:${saniye}`;
+  return sonuc;
+};
+
+audio.addEventListener("loadedmetadata", () => {
+  duration.textContent = calculateTime(audio.duration);
+  progressBar.max = Math.floor(audio.duration);
+});
+
+audio.addEventListener("timeupdate", () => {
+  progressBar.value = Math.floor(audio.currentTime);
+  currentTime.textContent = calculateTime(progressBar.value);
+});
+
+progressBar.addEventListener("input", () => {
+    currentTime.textContent = calculateTime(progressBar.value);
+    audio.currentTime = progressBar.value;
+});
