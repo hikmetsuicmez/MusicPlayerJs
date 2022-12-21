6e11cd206cab6459f823f55350ff8415a95c7e9e
Hikmet Suicmez <suicmezhikmet1@gmail.com>
2022-12-21 16:45:49 +0300

    Design with bootstrap

--- app.js
+++ app.js
@@ -59,13 +59,13 @@ function nextMusic() {
 
 function pauseMusic() {
   container.classList.remove("playing");
-  play.classList = "fa-solid fa-play";
+  play.querySelector("i").classList = "fa-solid fa-play";
   audio.pause();
 }
 
 function playMusic() {
   container.classList.add("playing");
-  play.classList = "fa-solid fa-pause";
+  play.querySelector("i").classList = "fa-solid fa-pause";
   audio.play();
 }
 
