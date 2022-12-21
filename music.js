ad7893ca74bd26dea0076af2f2c4820c347fbf9f
Hikmet Suicmez <suicmezhikmet1@gmail.com>
2022-12-21 17:24:50 +0300

    new music and gitignore edit

--- music.js
+++ music.js
@@ -14,5 +14,8 @@ class Music {
 const MusicList = [
     new Music("Boşver", "Nilüfer", "1.jpeg", "1.mp3"),
     new Music("Bu da Geçer mi Sevgilim", "Yalın", "2.jpeg", "2.mp3"),
-    new Music("Aramızda Uçurumlar", "Suat Suna", "3.jpeg", "3.mp3")
+    new Music("Aramızda Uçurumlar", "Suat Suna", "3.jpeg", "3.mp3"),
+    new Music("Yolla", "Tarkan", "4.jpeg", "4.mp3"),
+    new Music("Sen", "İdo Tatlıses", "5.jpeg", "5.mp3"),
+    new Music("Bulutlara Esir Olduk", "Oğuzhan Koç", "6.jpeg", "6.mp3")
 ];
